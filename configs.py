c401d4c60abb1f8ef22469693ec85505dab093d0
Sid1st <97543215+Sid1st@users.noreply.github.com>
2024-06-25 13:31:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,18 +9,18 @@ import os
 
 class Config(object):
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	API_ID = int(os.environ.get("API_ID", 12345))
-	API_HASH = os.environ.get("API_HASH")
+	API_ID = int(os.environ.get("API_ID", 28908582))
+	API_HASH = os.environ.get("API_HASH", "0d79d297bb8f56caed2c8f08bfc17289")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002219645512"))
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002151777689")
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-	CAPTION = "By @AHToolsBot"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	OWNER_ID = int(os.environ.get("OWNER_ID", 5052476013))
+	CAPTION = "By @MoviesXonee"
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Watermark_Bot")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://subhamgupta992sg:UXRT66yXTlP7MTJt@cluster0.bza4jw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", False))
 	USAGE_WATERMARK_ADDER = """
@@ -29,9 +29,9 @@ Hi, I am Video Watermark Adder Bot!
 **How to Added Watermark to a Video?**
 **Usage:** First Send a JPG Image/Logo, then send any Video. Better add watermark to a MP4 or MKV Video.
 
-__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/linux_repo).__
+__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/MoviesXonee).__
 
-Desgined by @AbirHasan2005
+Desgined by @MoviesXonee
 """
 	PROGRESS = """
 Percentage : {0}%
